1dba86e149a9c4555a3158794f53d8d827945984
Ricefab02 <roger.fabrice02@gmail.com>
2020-07-22 15:58:00 +0200

    build(bordertop.jsx): create bordertop with name site

---
+++ src/components/BorderTop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './BorderTop.css';
+
+function BorderTop() {
+    return (
+      <div className="">
+        <div className="BorderTop">
+          <p>BENPHONE</p>
+        </div>
+        
+        </div>
+      
+    );
+  }
+  
+  export default BorderTop;
